f01a7aae4af24ef773df8b3e694e7b30582dcefa
negiseijin <satoru6221@gmail.com>
2023-12-02 11:11:22 +0900

    add lint gradle9

--- app/lint.gradle.kts
+++ app/lint.gradle.kts
@@ -21,9 +21,9 @@ val ktlintCheck by tasks.registering(JavaExec::class) {
     )
 }
 
-// tasks.check {
-//    dependsOn(ktlintCheck)
-// }
+tasks.named("check") {
+    dependsOn(ktlintCheck)
+}
 
 tasks.register<JavaExec>("ktlintFormat") {
     group = LifecycleBasePlugin.VERIFICATION_GROUP
