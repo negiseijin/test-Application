6a70129ead1937ec7f3708727b2c747e1dbb7567
negiseijin <satoru6221@gmail.com>
2023-12-02 13:21:04 +0900

    add lint gradle (#3)
    * add lint gradle
    
    * add lint gradle2
    
    * add lint gradle3
    
    * add lint gradle4
    
    * add lint gradle5
    
    * add lint gradle6
    
    * add lint gradle7
    
    * add lint gradle8
    
    * add lint gradle9
    
    * add lint gradle10
    
    * add lint gradle11
    
    * add lint gradle12

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,7 +26,7 @@ android {
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
+                "proguard-rules.pro",
             )
         }
     }
@@ -50,14 +50,7 @@ android {
     }
 }
 
-//val ktlint by configurations.creating
-
 dependencies {
-//    ktlint("com.pinterest.ktlint:ktlint-cli:1.0.1") {
-//        attributes {
-//            attribute(Bundling.BUNDLING_ATTRIBUTE, objects.named(Bundling.EXTERNAL))
-//        }
-//    }
     implementation(libs.core.ktx)
     implementation(libs.lifecycle.runtime.ktx)
     implementation(libs.activity.compose)
@@ -75,34 +68,4 @@ dependencies {
     debugImplementation(libs.ui.test.manifest)
 }
 
-//val ktlintCheck by tasks.registering(JavaExec::class) {
-//    group = LifecycleBasePlugin.VERIFICATION_GROUP
-//    description = "Check Kotlin code style"
-//    classpath = ktlint
-//    mainClass.set("com.pinterest.ktlint.Main")
-//    // see https://pinterest.github.io/ktlint/install/cli/#command-line-usage for more information
-//    args(
-//        "**/src/**/*.kt",
-//        "**.kts",
-//        "!**/build/**",
-//    )
-//}
-//
-//tasks.check {
-//    dependsOn(ktlintCheck)
-//}
-//
-//tasks.register<JavaExec>("ktlintFormat") {
-//    group = LifecycleBasePlugin.VERIFICATION_GROUP
-//    description = "Check Kotlin code style and format"
-//    classpath = ktlint
-//    mainClass.set("com.pinterest.ktlint.Main")
-//    jvmArgs("--add-opens=java.base/java.lang=ALL-UNNAMED")
-//    // see https://pinterest.github.io/ktlint/install/cli/#command-line-usage for more information
-//    args(
-//        "-F",
-//        "**/src/**/*.kt",
-//        "**.kts",
-//        "!**/build/**",
-//    )
-//}
+apply(from = "${projectDir }/lint.gradle.kts")
---
+++ app/lint.gradle.kts
@@ -0,0 +1,41 @@
+val ktlint by configurations.creating
+
+dependencies {
+    ktlint("com.pinterest.ktlint:ktlint-cli:${libs.versions.ktlint.get()}") {
+        attributes {
+            attribute(Bundling.BUNDLING_ATTRIBUTE, objects.named(Bundling.EXTERNAL))
+        }
+    }
+}
+
+val ktlintCheck by tasks.registering(JavaExec::class) {
+    group = LifecycleBasePlugin.VERIFICATION_GROUP
+    description = "Check Kotlin code style"
+    classpath = ktlint
+    mainClass.set("com.pinterest.ktlint.Main")
+    // see https://pinterest.github.io/ktlint/install/cli/#command-line-usage for more information
+    args(
+        "**/src/**/*.kt",
+        "**.kts",
+        "!**/build/**",
+    )
+}
+
+tasks.named("check") {
+    dependsOn(ktlintCheck)
+}
+
+tasks.register<JavaExec>("ktlintFormat") {
+    group = LifecycleBasePlugin.VERIFICATION_GROUP
+    description = "Check Kotlin code style and format"
+    classpath = ktlint
+    mainClass.set("com.pinterest.ktlint.Main")
+    jvmArgs("--add-opens=java.base/java.lang=ALL-UNNAMED")
+    // see https://pinterest.github.io/ktlint/install/cli/#command-line-usage for more information
+    args(
+        "-F",
+        "**/src/**/*.kt",
+        "**.kts",
+        "!**/build/**",
+    )
+}
